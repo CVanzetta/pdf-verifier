44dba23a421dc14e1f0dd5308700ca67ae901d7d
Charles Vanzetta <charles.vanzetta@gmail.com>
2024-12-16 15:39:27 +0100

    ajout de Tailwind CSS

--- pdf-verifier/src/main.js
+++ pdf-verifier/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import Material from '@primevue/themes/material';
 import PrimeVue from 'primevue/config'
+import './main.css'
 
 const app = createApp(App);
 
