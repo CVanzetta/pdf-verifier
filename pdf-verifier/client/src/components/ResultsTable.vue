45731af9f96f7db4a7db9a8b2374ac733cbc71ec
Charles Vanzetta <charles.vanzetta@gmail.com>
2025-02-21 09:37:26 +0100

    ResultTable

--- pdf-verifier/client/src/components/ResultsTable.vue
+++ pdf-verifier/client/src/components/ResultsTable.vue
@@ -1,40 +1,41 @@
 <template>
     <Card header="Résultats des tests" class="mb-5">
-    <template #content>
-        <DataTable :value="results">
-        <Column field="status" header="Statut">
+      <template #content>
+        <DataTable :value="results" class="p-datatable-sm">
+          <Column field="status" header="Statut">
             <template #body="{ data }">
-            <i
+              <i
                 v-if="data.status === 'Passed'"
                 class="pi pi-check-circle"
                 style="color: green;"
                 title="Réussi"
-            ></i>
-            <i
+              ></i>
+              <i
                 v-else-if="data.status === 'Failed'"
                 class="pi pi-times-circle"
                 style="color: red;"
                 title="Échoué"
-            ></i>
+              ></i>
             </template>
-        </Column>
-        <Column field="categorie" header="Catégorie" />
-        <Column field="article" header="Article" />
-        <Column field="comments" header="Commentaires" />
+          </Column>
+          <Column field="categorie" header="Catégorie" />
+          <Column field="article" header="Article" />
+          <Column field="comments" header="Commentaires" />
         </DataTable>
-    </template>
+      </template>
     </Card>
-</template>
-
-<script setup>
-import Card from 'primevue/card';
-import DataTable from 'primevue/datatable';
-import Column from 'primevue/column';
-
-const props = defineProps({
+  </template>
+  
+  <script setup>
+  import Card from 'primevue/card';
+  import DataTable from 'primevue/datatable';
+  import Column from 'primevue/column';
+  
+  const props = defineProps({
     results: {
-    type: Array,
-    default: () => []
-    }
-});
-</script>
+      type: Array,
+      default: () => [],
+    },
+  });
+  </script>
+  
